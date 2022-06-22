318d44c913bb6d1b8cf16bb47ed9101036efd1f6
Grant-Syt <grantsyt21@gmail.com>
2022-06-22 18:56:44 -0400

    code style, new impls

--- sherlock_and_anagrams.py
+++ sherlock_and_anagrams.py
@@ -1,8 +1,9 @@
 from collections import Counter
+from itertools import count
 
 def main():
     s = "cdcd"
-    print(f"My Solution: {sherlockAndAnagrams_mySolution(s)}")
+    # print(f"My Solution: {sherlockAndAnagrams_mySolution(s)}")
     print(f"Example Solution: {sherlockAndAnagrams_exampleSolution(s)}")
     print("Expected: 5")
 
@@ -44,37 +45,62 @@ def sherlockAndAnagrams_mySolution(s):
     
     return count
 
-def sherlockAndAnagrams_exampleSolution(s):
+def sherlockAndAnagrams_exampleSolution(str_input):
     
-    def all_substrs(s):
+    # find substrs with list comprehension
+    def all_substrs_list_comp(str_input):
         
-              #   (3)   (2)                             (1)
-        ss = [[s[j:j+i] for j in range(len(s) - i + 1)] for i in range(1, len(s))]
-        # 1. outer loop - i is the length of the slice, 1-(len(s)-1), 
-        #                   stops before the full string,
+              #           (3)                                        (2)
+        sub_strs = [[str_input[j:j+i] for j in range(len(str_input) - i + 1)] 
+                                for i in range(1, len(str_input))] # (1)
+        # 1. outer loop - i is the length of the slice; 
+        #                   stops before the full string;
         #                   makes list of lists
-        # 2. inner loop - j is the start index of the slice, 0-(len(s)-i+1),
-        #                   stops before the first out of range slice,
-        #                   the +1 is because the stop is non-inclusive,
+        # 2. inner loop - j is the start index of the slice;
+        #                   stops before the first out of range slice;
+        #                   the +1 is because the stop is non-inclusive;
         #                   makes list
-        # 3. function of variables - slice s from j to j+i
+        # 3. slice s from j to j+i
         
         # print(ss)
-        return ss
+        return sub_strs
 
-    def countem(ll):
-        c = Counter()
-        s = 0
-        for lst in ll:
-            for e in lst:
-                q = ''.join(sorted(e))
-                c[q] += 1   # count sorted strings
+    # find substrs, easier to read
+    def all_substrs(str_input):
+        sub_strs = []
+        n = len(str_input)
+        for i in range(1, n): # sub string length
+            arr = []
+            for j in range(n - i + 1): # starting points for slicing
+                arr.append(str_input[j:j+i])
+            sub_strs.append(arr)
+        return sub_strs
+
+    def all_subarry(arr):
+        res = [arr[i:j] for i in range(len(arr)) for j in range(i+1, len(arr)+1)]
+        return res
+
+    # count anaagrams by sorting substrs
+    def count_anagrams_sort_impl(sub_strs):
+        anagram_counts = Counter()
+        count = 0
+        for i in sub_strs:
+            for i in i:
+                q = ''.join(sorted(i))
+                anagram_counts[q] += 1   # count sorted strings
         # print(c)
-        for e in c:
-            s += int(c[e]*(c[e]-1)/2) # simplified "n choose 2"
-        return s
+        for i in anagram_counts:
+            count += int(anagram_counts[i]*(anagram_counts[i]-1)/2) # simplified "n choose 2"
+        return count
 
-    return countem(all_substrs(s))
+    # count anagrams by comparing character counts, faster
+    # def count_anagrams_char_count_impl():
+
+    print(all_substrs(str_input))
+    print(all_substrs_list_comp(str_input))
+    print(all_subarry(str_input))
+
+    return count_anagrams_sort_impl(all_substrs(str_input))
     
 
 if __name__ == "__main__":
